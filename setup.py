54278acdce97c5362e303f116650e0b918484bf2
skjerns <simon.kern@online.de>
2017-07-28 13:10:02 +0200

    init

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='sdill',
+      version='0.1',
+      description='A Dill wrapper to call dill with file strings',
+      url='http://github.com/skjerns/sDill',
+      author='skjerns',
+      author_email='nomail',
+      license='GNU',
+      packages=['sdill'],
+      zip_safe=False)
