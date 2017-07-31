2e349777f6013a51f30317e7f8e123d7c398dffc
skjerns <simon.kern@online.de>
2017-07-31 14:50:27 +0200

    added dill dependency

--- setup.py
+++ setup.py
@@ -1,11 +1,12 @@
 from setuptools import setup
 
 setup(name='sdill',
-      version='0.19',
+      version='0.20',
       description='A Dill wrapper to call dill with file strings',
       url='http://github.com/skjerns/sDill',
       author='skjerns',
       author_email='nomail',
       license='GNU',
       packages=['sdill'],
+      install_requires=['dill',],
       zip_safe=False)
