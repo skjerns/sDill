72580a08c21c992fc4339bb14c4b6704e69cf9c3
skjerns <simon.kern@online.de>
2017-07-31 14:23:35 +0200

    changed import behaviour

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='sdill',
-      version='0.14',
+      version='0.15',
       description='A Dill wrapper to call dill with file strings',
       url='http://github.com/skjerns/sDill',
       author='skjerns',
